374d4a2108c70fe90a275a7a5c14cddd9e561ac9
Abena <72131115+BlackPhinix@users.noreply.github.com>
2022-07-03 19:10:48 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+from  cs1robots import*
+load_world("worlds/harvest4.wld")
+hubo = Robot()
+hubo.set_trace("blue")
+hubo.set_speed(1)
+hubo.set_position(0,0)
+hubo.set_orientation(0)
+hubo.set_noise(0.0)
+hubo.set_camera_orientation(0)
